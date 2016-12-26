95d52be772814d5c1bf18f8ad6e95c240cc30839
Ethan Swan <eswan18@rocketmail.com>
2016-12-26 13:32:22 -0500

    cleanup, commenting

--- main.py
+++ main.py
@@ -9,29 +9,32 @@ import time
 
 def game_loop():
 
-    board = GameBoard()
-    winner = None
-    n_turns = 0
+    board = GameBoard() #initialize a board
+    winner = None #initialize the winner variable
+    n_turns = 0 #initialize the turn count
+
+    #Run the game - looping until someone wins
     while (winner == None):
         board.print_board()
         
-        #Loop until valid input
-        is_valid = False
-
         #Figure out the current player and color
         if (n_turns % 2): #if there have been an odd number of turns
-            player = p2
-            color = 'Y'
-        else:
+            player = p2 #P2 makes the next move
+            color = 'Y' #and the color is yellow
+        else: #if there have been an even number of moves...you get it
             player = p1
             color = 'R'
+
+        #Loop until valid input
+        is_valid = False
         while (not is_valid):
-            x,y = player.make_move(board) #P2 makes the next move
+            x,y = player.make_move(board) #call the player's move function 
             is_valid = board.check_valid(x, y, color)
 
+	#Place the piece and check for a winner
         board.place_piece(x, y, color)
         winner = board.check_for_winner(color)
-        time.sleep(1)
 
+#Driver
 if __name__ == '__main__':
     game_loop()
