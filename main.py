ca74dcd8e0e321f0e321e74b50125cf6113e1f9a
Ethan Swan <eswan18@rocketmail.com>
2016-12-26 13:28:10 -0500

    Updated main file to use 2 player classes

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from gameboard import GameBoard
-import player1
+import player1 as p1
+import player1 as p2
+import time
+
+
+#Note: P1 is red, P2 is yellow
 
 
 def game_loop():
 
     board = GameBoard()
     winner = None
+    n_turns = 0
     while (winner == None):
         board.print_board()
         
         #Loop until valid input
         is_valid = False
+
+        #Figure out the current player and color
+        if (n_turns % 2): #if there have been an odd number of turns
+            player = p2
+            color = 'Y'
+        else:
+            player = p1
+            color = 'R'
         while (not is_valid):
-            x,y = player1.make_move(board)
-            is_valid = board.check_valid(x, y, 'R')
-          
-        board.place_piece(x, y, 'R')
-        winner = board.check_for_winner('R')
+            x,y = player.make_move(board) #P2 makes the next move
+            is_valid = board.check_valid(x, y, color)
+
+        board.place_piece(x, y, color)
+        winner = board.check_for_winner(color)
+        time.sleep(1)
 
 if __name__ == '__main__':
     game_loop()
--- player1.py
+++ player1.py
@@ -2,7 +2,4 @@ from gameboard import GameBoard
 
 
 def make_move(board):
-    #board.place_piece(1, 6, 'R')
-    #board.place_piece(1, 5, 'R')
-    #board.place_piece(1, 4, 'R')
-    return (1,1)
+    return (1,6)
