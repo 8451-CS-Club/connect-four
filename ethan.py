48919d6c7cf64b47ee60912a498b2590beeb83f0
eswan18 <eswan18@rocketmail.com>
2017-04-30 00:42:48 -0400

    Cleaned up Ethan player. Still broken

--- ethan.py
+++ ethan.py
@@ -2,84 +2,73 @@ from gameboard import GameBoard
 from time import sleep
 import copy
 
-
+# Make a move
 def make_move(board, color):
     best_move_x = 0
-    best_val = -2 #This value being lower than -1 means the player will prefer an empty space over nothing, even if it means losing
+    # This value being lower than -1 means the player will prefer an
+    # empty space over nothing, even if it means losing
+    best_val = -2 
     return(find_best_move(board, color))
 
+
+# Find the best move for a player given a current board state
+def find_best_move(board, color):
+    # Initialize the best move value at -2 because even a losing
+    # move (with a value of -1) is better than doing nothing (-2)
+    best_move_value = -2
+    best_move_col = -1
+    # Check the value of placing a piece in each column
     for x in range(board.columns):
-        print("looping on x: ",x)
-        open_y = open_row(board, x)
-        print(open_y)
-        if (open_y == -1):
-            #only keep this value if we haven't found an open space yet
-            if (best_val == -2):
-                best_move_x = x
-            continue
-        new_board = copy.deepcopy(board)
-        new_board.board[open_y][x] = color
-        #new_board.print_board()
-        current_val = board_val(new_board, color)
-        if (current_val > best_val):
-            print("updating best move")
-            best_val = current_val
-            best_move_x = x
-        print("finished looping on x: ", x)
-    return(best_move_x)
+        # Get the x-coordinate of where a piece would be placed
+        y = open_row(board, x)
+        # Copy the board and add a piece in the correct position
+        hypothetical_board = copy.deepcopy(board)
+        hypothetical_board.board[y][x] = color
+        # Get the value of the board resulting from placing a pice here
+        hypothetical_value = board_val(hypothetical_board, color)
+        # If this is better than the current best move, remember it
+        if hypothetical_value > best_move_value:
+            best_move_value = hypothetical_value
+            best_move_col = x
+    return(best_move_col)
+
 
 # Recursive function to figure out who will win in
-# this situation given optimal play
+# this situation given that the opponent moves next
 def board_val(board, color):
     board_val.counter += 1
     if (not (board_val.counter % 10000)):
         print(board_val.counter)
+
     # Exit conditions first
     winner = board.check_for_winner(color)
-    print(winner)
     if (winner == color):
-        print(color + "got to a win")
         return 1
-    # I don't think there's any way to have lost when this is called, beacuse you'd
-    # the player who calls this has just made her move
-    #elif (winner is not None):
-    #    print(color + "got to a loss")
-    #    return -1
+    elif (winner is not None):
+        return -1
     else:
         max_opp_board_val = -1
         opp_move_x = -1
         opp_move_y = -1
-        for x in range(board.columns):
-            #Find the first open spot in that column
-            open_y = open_row(board, x)
-            if (open_y == -1):
-                continue #skip to the next column
-            opp_color = 'R' if (color == 'Y') else 'Y' #concisely get opponent's color
-            # Create a new board with an opponent's piece in the selected spot
-            new_board = copy.deepcopy(board)
-            new_board.board[open_y][x] = opp_color
-            #Calculate this board's value to your opponent
-            current_board_val = board_val(new_board, opp_color)
-            # If you find a board where your opponent always wins
-            if (current_board_val == 1):
-                return(-1) # then you always lose (duh)
-            #Otherwise, if it's the best one you've found for your opponent, remember it
-            if current_board_val > max_opp_board_val:
-                max_opp_board_val = current_board_val
-                opp_move_y = open_y
-                opp_move_x = x
-        # Now that we've checked all the columns, we know the best move for our opponent
-        # Return the opposite value of that move's value to her
-        return(-1 * max_opp_board_val)
 
+        # Determine the value of the board that will result after opponent
+        # makes her optimal move
+        opp_color = 'R' if (color == 'Y') else 'Y' #concisely get opponent's color
+        opp_move = find_best_move(board, opp_color)
+        # Create a board and add this opponent move
+        opp_move_board = copy.deepcopy(board)
+        opp_y_pos = open_row(opp_move_board, opp_move)
+        opp_move_board.board[opp_y_pos][opp_move] = opp_color
+        # Return the oppositve of the value of this board to your opponent
+        return -1 * board_val(opp_move_board, opp_color)
+
+# Counter for how many times this function is called
 board_val.counter = 0
 
 
-find_best_move
-
 
 # Get the first open row in a column
-# Return -1 if no opening
+# Return -1 if the column is full
 def open_row(board, column):
     if (column >= board.columns or column < 0):
         print("Error, invalid column specified")
